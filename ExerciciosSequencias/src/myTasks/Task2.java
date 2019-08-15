91cee0ebf84a3ba3eb351b039945fe7ae5768500
vcalazas <vitor.dalmolin@grupoabl.com.br>
2019-08-15 15:55:47 -0300

    .

--- ExerciciosSequencias/src/myTasks/Task2.java
+++ ExerciciosSequencias/src/myTasks/Task2.java
@@ -6,10 +6,10 @@ public class Task2 extends MyTask{
 	public void run() {
 		try {
 			// TODO Auto-generated method stub
-			// 2 - Dado que a sequencia S é iterada de acordo com an = 2n-1.  Imprima os 10 primeiros valores
+			// 2 - Dado que a sequencia S ï¿½ iterada de acordo com an = 2n-1.  Imprima os 10 primeiros valores
 			int an = 1;
 			for(int i = 1; i <= 10; i++) {
-				an = 2 * 2 * an-1;
+				an = 2 * an-1;
 				MyTaskUtil.say( an+( i == 10 ? "" : " - " ));
 			}
 		} catch(Exception e) {
