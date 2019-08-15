06e3bdbab605cc0c58114e85e81683a3311bf00c
vcalazas <vitor.dalmolin@grupoabl.com.br>
2019-08-15 15:53:37 -0300

    Correção

--- ExerciciosSequencias/src/myTasks/Task2.java
+++ ExerciciosSequencias/src/myTasks/Task2.java
@@ -7,8 +7,10 @@ public class Task2 extends MyTask{
 		try {
 			// TODO Auto-generated method stub
 			// 2 - Dado que a sequencia S é iterada de acordo com an = 2n-1.  Imprima os 10 primeiros valores
+			int an = 1;
 			for(int i = 1; i <= 10; i++) {
-				MyTaskUtil.say(( (2 * i) - 1 )+( i == 10 ? "" : " - " ));
+				an = 2 * 2 * an-1;
+				MyTaskUtil.say( an+( i == 10 ? "" : " - " ));
 			}
 		} catch(Exception e) {
 			MyTaskUtil.say("ERROR - TASK2");
